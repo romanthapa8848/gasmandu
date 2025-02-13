c3a90e562c71fbc13fb2afd22bcb3cbe92fe8e5a
romanthapa8848 <romanthapa8848@gmail.com>
2025-02-13 16:11:52 +0545

    order page

--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart' as geocoding;
 import 'package:intl/intl.dart'; // Import the intl package for date formatting
+import 'package:flutter/services.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -108,8 +109,6 @@ class _OrderScreenState extends State<OrderScreen> {
     if (picked != null) {
       setState(() {
         _selectedDate = picked;
-
-        // Format the date using DateFormat from the intl package
         _dateController.text = DateFormat('yyyy-MM-dd').format(picked);
       });
     }
@@ -157,176 +156,187 @@ class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: const Text('Place Order',
-            style: TextStyle(fontWeight: FontWeight.bold)),
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.help_outline),
-            onPressed: () {
-              // Help icon action
-            },
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Gas Brand Dropdown
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(
-                  labelText: 'Select Gas Brand',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                ),
-                items: _gasBrands
-                    .map((brand) => DropdownMenuItem<String>(
-                          value: brand,
-                          child: Text(brand),
-                        ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedGasBrand = value;
-                  });
-                },
-                validator: (value) =>
-                    value == null ? 'Please select a gas brand' : null,
-              ),
-              const SizedBox(height: 20),
-              // Delivery Address (read-only)
-              TextFormField(
-                controller: _addressController,
-                readOnly: true,
-                decoration: const InputDecoration(
-                  labelText: 'Delivery Address',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                ),
-              ),
-              const SizedBox(height: 20),
-              // Phone Number Input
-              TextFormField(
-                controller: _phoneController,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                  labelText: 'Phone Number',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                ),
-                validator: (value) => value == null || value.isEmpty
-                    ? 'Enter phone number'
-                    : null,
-              ),
-              const SizedBox(height: 20),
-              // Floor Selection Dropdown
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(
-                  labelText: 'Select Floor',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                ),
-                value: _selectedFloor,
-                items: _floors
-                    .map((floor) => DropdownMenuItem<String>(
-                          value: floor,
-                          child: Text(floor),
-                        ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedFloor = value!;
-                  });
-                },
-                validator: (value) =>
-                    value == null ? 'Please select a floor' : null,
-              ),
-              const SizedBox(height: 20),
-              // Delivery Date Selection (Date Picker)
-              TextFormField(
-                controller: _dateController,
-                readOnly: true,
-                decoration: const InputDecoration(
-                  labelText: 'Delivery Date',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                ),
-                onTap: () => _selectDate(context),
-                validator: (value) => value == null || value.isEmpty
-                    ? 'Select a delivery date'
-                    : null,
-              ),
-              const SizedBox(height: 20),
-              // Payment Method (Radio Buttons)
-              const Text(
-                'Payment Method',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Column(
-                children: [
-                  RadioListTile<String>(
-                    title: const Text('Esewa'),
-                    value: 'Esewa',
-                    groupValue: _paymentMethod,
-                    onChanged: (value) {
-                      setState(() {
-                        _paymentMethod = value!;
-                      });
-                    },
+      // AppBar removed so there's no top "Place Order" section
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Gas Brand Dropdown
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(
+                    labelText: 'Select Gas Brand',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
-                  RadioListTile<String>(
-                    title: const Text('Khalti'),
-                    value: 'Khalti',
-                    groupValue: _paymentMethod,
-                    onChanged: (value) {
-                      setState(() {
-                        _paymentMethod = value!;
-                      });
-                    },
-                  ),
-                  RadioListTile<String>(
-                    title: const Text('Cash on Delivery'),
-                    value: 'Cash on Delivery',
-                    groupValue: _paymentMethod,
-                    onChanged: (value) {
-                      setState(() {
-                        _paymentMethod = value!;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              // Submit Order Button
-              ElevatedButton(
-                onPressed: _submitOrder,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent, // Corrected parameter
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                  items: _gasBrands
+                      .map((brand) => DropdownMenuItem<String>(
+                            value: brand,
+                            child: Text(brand),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedGasBrand = value;
+                    });
+                  },
+                  validator: (value) =>
+                      value == null ? 'Please select a gas brand' : null,
+                ),
+                const SizedBox(height: 20),
+                // Delivery Address (read-only)
+                TextFormField(
+                  controller: _addressController,
+                  readOnly: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Delivery Address',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
                 ),
-                child: const Text('Place Order'),
-              ),
-            ],
+                const SizedBox(height: 20),
+                // Phone Number Input
+                TextFormField(
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly, // Allow only digits
+                    LengthLimitingTextInputFormatter(
+                        10), // Limit input to 10 characters
+                  ],
+                  decoration: const InputDecoration(
+                    labelText: 'Phone Number',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter phone number';
+                    } else if (value.length != 10) {
+                      return 'Phone number must be 10 digits';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                // Floor Selection Dropdown
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(
+                    labelText: 'Select Floor',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  ),
+                  value: _selectedFloor,
+                  items: _floors
+                      .map((floor) => DropdownMenuItem<String>(
+                            value: floor,
+                            child: Text(floor),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedFloor = value!;
+                    });
+                  },
+                  validator: (value) =>
+                      value == null ? 'Please select a floor' : null,
+                ),
+                const SizedBox(height: 20),
+                // Delivery Date Selection (Date Picker)
+                TextFormField(
+                  controller: _dateController,
+                  readOnly: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Delivery Date',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  ),
+                  onTap: () => _selectDate(context),
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Select a delivery date'
+                      : null,
+                ),
+                const SizedBox(height: 20),
+                // Payment Method (Radio Buttons)
+                const Text(
+                  'Payment Method',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Column(
+                  children: [
+                    RadioListTile<String>(
+                      title: const Text('Esewa'),
+                      value: 'Esewa',
+                      groupValue: _paymentMethod,
+                      onChanged: (value) {
+                        setState(() {
+                          _paymentMethod = value!;
+                        });
+                      },
+                    ),
+                    RadioListTile<String>(
+                      title: const Text('Khalti'),
+                      value: 'Khalti',
+                      groupValue: _paymentMethod,
+                      onChanged: (value) {
+                        setState(() {
+                          _paymentMethod = value!;
+                        });
+                      },
+                    ),
+                    RadioListTile<String>(
+                      title: const Text('Cash on Delivery'),
+                      value: 'Cash on Delivery',
+                      groupValue: _paymentMethod,
+                      onChanged: (value) {
+                        setState(() {
+                          _paymentMethod = value!;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+// Submit Order Button (bottom of the body)
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _submitOrder,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Color(0xFFFF6F61), // Gas cylinderish reddish light
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 18),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(30), // More curvy button
+                      ),
+                      elevation: 5, // Slight shadow for depth
+                    ),
+                    child: const Text(
+                      'Place Order',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // Ensures good contrast
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
